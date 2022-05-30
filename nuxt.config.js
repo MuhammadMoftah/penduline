e0c69027dd691002725aff897f85688af8e6bb32
MuhammadMoftah <muhammad.moftah3787@gmail.com>
2022-05-31 00:39:08 +0200

    change fav icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" },
     {
       rel: "stylesheet",
       href: "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap",
